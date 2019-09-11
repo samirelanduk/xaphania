588abb1e77ada4937259e67fce27b8606cde6c23
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-11 23:33:13 +0100

    Add Field type

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -4,6 +4,12 @@ import xaphania
 class Test(TestCase):
 
     def test(self):
+        string = xaphania.ObjectType(name="String")
+
+
         query = xaphania.ObjectType(
-         name="Query"
+         name="Query",
+         fields=[
+          xaphania.Field(name="version", type=string)
+         ]
         )
--- xaphania.py
+++ xaphania.py
@@ -1,11 +1,24 @@
 class ObjectType:
     
-    def __init__(self, name):
+    def __init__(self, name, fields=None):
         self.name = name
+        self.fields = fields or []
     
 
     def __repr__(self):
         return f"<{self.name} Type>"
+
+
+
+class Field:
+    
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+
+    def __repr__(self):
+        return f"<{self.name} Field ({self.type.name})>"
 
 
 
