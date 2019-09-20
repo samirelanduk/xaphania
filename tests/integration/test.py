b1646e4210f44e9e2fca7468638728f526e2b0fc
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-20 12:41:58 +0100

    Field adding

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -6,10 +6,11 @@ class Test(TestCase):
     def test(self):
         string = xaphania.ObjectType(name="String")
 
+        query = xaphania.ObjectType(name="Query")
+        person = xaphania.ObjectType(name="Person")
 
-        query = xaphania.ObjectType(
-         name="Query",
-         fields=[
-          xaphania.Field(name="version", type=string)
-         ]
-        )
+        query.add_field("leader", person)
+
+        person.add_field("name", string)
+
+        
--- xaphania.py
+++ xaphania.py
@@ -1,20 +1,24 @@
 class ObjectType:
     
-    def __init__(self, name, fields=None):
+    def __init__(self, name):
         self.name = name
-        self.fields = fields or []
+        self.fields = []
     
 
     def __repr__(self):
         return f"<{self.name} Type>"
+    
+
+    def add_field(self, field_name, object_type):
+        self.fields.append(Field(field_name, object_type))
 
 
 
 class Field:
     
-    def __init__(self, name, type):
+    def __init__(self, name, object_type):
         self.name = name
-        self.type = type
+        self.type = object_type
     
 
     def __repr__(self):
