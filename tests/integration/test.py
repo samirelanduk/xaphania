63646fbc6a421a23218139b5966a2892145e81ec
Sam Ireland <sam.ireland.uk@gmail.com>
2019-10-02 09:25:32 +0100

    Add Argument class

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -5,11 +5,16 @@ class Test(TestCase):
 
     def test(self):
         string = xaphania.ObjectType(name="String")
+        integer = xaphania.ObjectType(name="Integer")
 
         query = xaphania.ObjectType(name="Query")
         person = xaphania.ObjectType(name="Person")
 
         query.add_field("leader", person)
+        query.add_field(
+         "person", person, arguments=[xaphania.Argument("id", integer)]
+        )
+        print(query.fields[1].arguments[0])
 
         person.add_field("name", string)
 
--- xaphania.py
+++ xaphania.py
@@ -9,20 +9,37 @@ class ObjectType:
         return f"<{self.name} Type>"
     
 
-    def add_field(self, field_name, object_type):
-        self.fields.append(Field(field_name, object_type))
+    def add_field(self, field_name, object_type, arguments=None):
+        arguments = arguments or []
+        self.fields.append(Field(field_name, object_type, arguments))
 
 
 
 class Field:
     
-    def __init__(self, name, object_type):
+    def __init__(self, name, object_type, arguments):
         self.name = name
         self.type = object_type
+        self.arguments = arguments
     
 
     def __repr__(self):
         return f"<{self.name} Field ({self.type.name})>"
+
+
+
+class Argument:
+
+    def __init__(self, name, object_type, required=False):
+        self.name = name
+        self.type = object_type
+        self.required = required
+    
+
+    def __repr__(self):
+        return "<{} {} argument ({}required)>".format(
+         self.name, self.type.name, "" if self.required else "not "
+        )
 
 
 
