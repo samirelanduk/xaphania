53ef0938cb041b9aeb6226738b699ec54392c38b
Sam Ireland <sam.ireland.uk@gmail.com>
2019-10-07 12:41:23 +0100

    Basic schema representations

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -4,19 +4,26 @@ import xaphania
 class Test(TestCase):
 
     def test(self):
+        query = xaphania.ObjectType(name="Query")
+
+
         string = xaphania.ObjectType(name="String")
         integer = xaphania.ObjectType(name="Integer")
 
-        query = xaphania.ObjectType(name="Query")
+        
         person = xaphania.ObjectType(name="Person")
+        person.add_field("name", string)
+        person.add_field("age", integer, nullable=True)
+
+        
 
         query.add_field("leader", person)
         query.add_field(
-         "person", person, arguments=[xaphania.Argument("id", integer)]
+         "person", person, arguments=[xaphania.Argument("id", integer, required=True)]
         )
 
-        person.add_field("name", string)
-
+        print(query.schema_repr())
+        print(person.schema_repr())
         schema = xaphania.Schema(query)
 
 
--- xaphania.py
+++ xaphania.py
@@ -9,22 +9,39 @@ class ObjectType:
         return f"<{self.name} Type>"
     
 
-    def add_field(self, field_name, object_type, arguments=None):
+    def add_field(self, field_name, object_type, arguments=None, nullable=False):
         arguments = arguments or []
-        self.fields.append(Field(field_name, object_type, arguments))
+        self.fields.append(Field(field_name, object_type, arguments, nullable))
+    
+
+    def schema_repr(self):
+        return "type {} {{\n  {}\n}}".format(
+         self.name,
+         "\n  ".join(f.schema_repr() for f in self.fields)
+        )
 
 
 
 class Field:
     
-    def __init__(self, name, object_type, arguments):
+    def __init__(self, name, object_type, arguments, nullable=False):
         self.name = name
         self.type = object_type
         self.arguments = arguments
+        self.nullable = nullable
     
 
     def __repr__(self):
         return f"<{self.name} Field ({self.type.name})>"
+
+    
+    def schema_repr(self):
+        return "{}{}: {}{}".format(
+         self.name,
+         "" if not self.arguments else\
+          f"({', '.join(a.schema_repr() for a in self.arguments)})",
+         self.type.name, "" if self.nullable else "!"
+        )
 
 
 
@@ -39,6 +56,12 @@ class Argument:
     def __repr__(self):
         return "<{} {} argument ({}required)>".format(
          self.name, self.type.name, "" if self.required else "not "
+        )
+    
+
+    def schema_repr(self):
+        return "{}: {}{}".format(
+         self.name, self.type.name, "!" if self.required else ""
         )
 
 
