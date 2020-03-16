45c7963b633d06a91a6c9424d561056b10b98ac5
Sam Ireland <sam.ireland.uk@gmail.com>
2020-03-16 13:40:35 +0000

    Restart with parsing

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -4,32 +4,4 @@ import xaphania
 class Test(TestCase):
 
     def test(self):
-        query = xaphania.ObjectType(name="Query")
-
-
-        string = xaphania.ObjectType(name="String")
-        integer = xaphania.ObjectType(name="Integer")
-
-        
-        person = xaphania.ObjectType(name="Person")
-        person.add_field("name", string)
-        person.add_field("age", integer, nullable=True)
-
-        
-
-        query.add_field("leader", person)
-        query.add_field(
-         "person", person, arguments=[xaphania.Argument("id", integer, required=True)]
-        )
-        query.add_field(
-         "people", person, list=True, arguments=[xaphania.Argument("age__gt", integer, required=False)]
-        )
-
-        schema = xaphania.Schema(query)
-
-
-        string = xaphania.QueryString("{ user }")
-        self.assertEqual(string.structure, {"user": None})
-        string = xaphania.QueryString("{ user version }")
-        self.assertEqual(string.structure, {"user": None, "version": None})
-        string = xaphania.QueryString("{ user { name } version }")
+        pass
